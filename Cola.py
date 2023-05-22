c0b391acec232ac18bd95b3d268db6e924142818
miguelromero1 <miguelromeroromero@cedoc.edu.co>
2023-05-21 19:38:38 -0500

    notebook

---
+++ Cola.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+class Cola:
+    def __init__(self):
+        self.items = deque()
+
+    def esta_vacia(self):
+        return len(self.items) == 0
+
+    def encolar(self, item):
+        self.items.append(item)
+
+    def desencolar(self):
+        if not self.esta_vacia():
+            return self.items.popleft()
+
+    def ver_frente(self):
+        if not self.esta_vacia():
+            return self.items[0]
---
+++ Pila.py
@@ -0,0 +1,17 @@
+class Pila:
+    def __init__(self):
+        self.items = []
+
+    def esta_vacia(self):
+        return len(self.items) == 0
+
+    def apilar(self, item):
+        self.items.append(item)
+
+    def desapilar(self):
+        if not self.esta_vacia():
+            return self.items.pop()
+
+    def ver_tope(self):
+        if not self.esta_vacia():
+            return self.items[-1]
